033b6109e0df9267ddd61ccd22d4355770d3dfb0
19Lida <lida19melnyk@gmail.com>
2022-12-13 21:19:39 +0200

    редагує

--- src/index.js
+++ src/index.js
@@ -100,23 +100,26 @@ function markupGallery(data) {
 async function onLoadMore() {
   newsApiService.incrementPage();
   // newsApiService
-  const result = await newsApiService
-    .fetchImage()
-    .then(({ hits, totalHits }) => {
+  const data = await newsApiService.fetchImage();
+  try {
+    ({ data: { hits, totalHits } }) => {
       console.log(totalHits);
       renderCard(hits);
       console.log(newsApiService.page);
-      console.log(totalPages);
+      // console.log(totalPages);
       if (newsApiService.page === totalPages) {
         refs.buttonLoad.classList.add('is-hidden');
         Notify.failure(
           'We are sorry, but you have reached the end of search results'
         );
       }
-    })
-    .catch(error => console.log(error));
-  await smoothScroll();
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
+// await smoothScroll();
+
 async function smoothScroll() {
   const { height: cardHeight } =
     refs.galleryEl.firstElementChild.getBoundingClientRect();
